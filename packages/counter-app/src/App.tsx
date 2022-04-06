c29d429999719ddc0899ed31c6c4e55c28a78526
hung-showwcase <hung@showwcase.com>
2022-04-06 20:16:02 +0700

    initial

--- packages/counter-app/src/App.tsx
+++ packages/counter-app/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useModel } from "remit";
+import { useModel } from "remos";
 const App = () => {
   const model = useModel(() => ({ count: 1 }));
   return <h1 onClick={() => model.count++}>{model.count}</h1>;
--- packages/remit/__tests__/remit.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const remit = require('..');
-
-describe('remit', () => {
-    it('needs tests');
-});
